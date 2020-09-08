f78ae4c84bed0616b0870c2f00b1658be5a77d39
Tiny999 <salamiabbas3@gmail.com>
2020-09-08 19:40:30 +0100

    build ui and data

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingFor: 'female',
+    location: 'Ilorin KW',
+    image: 'https://randomuser.me/api/portraits/men/23.jpg'
+  },
+  {
+    name: 'Katy Johnson',
+    age: 22,
+    gender: 'female',
+    lookingFor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/female/82.jpg'
+  },
+  {
+    name: 'Aboi Ahmad',
+    age: 29,
+    gender: 'male',
+    lookingFor: 'female',
+    location: 'Offa KW',
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+  }
+];
+
+// Profile Iterator
+function profileIterator
